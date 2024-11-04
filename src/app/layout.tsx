cbc22a902dbcdd3fe274c7b734776577de08f0ce
JuniorVinicius <jr.vinicius.silva@gmail.com>
2024-11-04 17:22:46 -0300

    chore: add initial settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
 import type { Metadata } from 'next'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from '@/theme'
+import { Roboto } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: 'Of Course',
   description: 'Of Course Platform',
 }
-
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+})
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -12,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={roboto.variable}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   )
 }
